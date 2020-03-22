592e9c2a19a86ee6eac2714eb0a88eb2cc261030
Jose Canova <jose.c.canova@gmail.com>
2020-03-22 16:52:15 -0300

    Added Transformer based abstraction

--- core-base/core-base/src/main/java/org/nanotek/opencsv/base/WrappedBaseBean.java
+++ core-base/core-base/src/main/java/org/nanotek/opencsv/base/WrappedBaseBean.java
@@ -8,6 +8,8 @@ import org.nanotek.opencsv.WrappedBaseClass;
 
 public class WrappedBaseBean<ID extends IdBase<?,?>> extends WrappedBaseClass<ID>{
 
+	private static final long serialVersionUID = 4115182730636624097L;
+
 	public WrappedBaseBean(IdBase<?,?> idBase) {
 		super(idBase);
 	}
---
+++ core-base/core-base/src/main/java/org/nanotek/tranformer/WrappedBaseBeanTransformer.java
@@ -0,0 +1,25 @@
+package org.nanotek.tranformer;
+
+import org.nanotek.BaseEntity;
+import org.nanotek.EntityBaseTransformer;
+import org.nanotek.IdBase;
+import org.nanotek.opencsv.base.WrappedBaseBean;
+
+public class WrappedBaseBeanTransformer<K extends IdBase<K,ID> , ID extends BaseEntity<?,?>> 
+extends WrappedBaseBean<ID> 
+implements EntityBaseTransformer<K, ID> {
+
+	private ID id;
+	
+	public WrappedBaseBeanTransformer(BaseEntity<?, ?> id) {
+		super(id);
+	}
+
+	private static final long serialVersionUID = -8048291218009448615L;
+
+	@Override
+	public ID transform(K i) {
+		return id;
+	}
+
+}
