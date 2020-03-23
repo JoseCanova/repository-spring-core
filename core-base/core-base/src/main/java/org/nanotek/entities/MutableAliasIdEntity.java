492364c74d9557a14eb1bb75883a516fb4ae9293
Jose Canova <jose.c.canova@gmail.com>
2020-03-23 01:11:22 -0300

    Fixing Imports.

--- core-base/core-base/src/main/java/org/nanotek/entities/MutableAliasIdEntity.java
+++ core-base/core-base/src/main/java/org/nanotek/entities/MutableAliasIdEntity.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 
 import org.nanotek.entities.immutables.AliasIdEntity;
 
-public interface MutableAliasIdEntity<K extends Serializable> extends AliasIdEntity<K extends Serializable>
+public interface MutableAliasIdEntity<K extends Serializable> 
+extends AliasIdEntity<K>
 
 	void setAliasId(K k);
 	
