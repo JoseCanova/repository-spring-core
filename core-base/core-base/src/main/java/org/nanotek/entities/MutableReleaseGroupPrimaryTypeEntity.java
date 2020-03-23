710a118dd745218565e6d4c6a3da336898faed82
Jose Canova <jose.c.canova@gmail.com>
2020-03-23 01:15:00 -0300

    Fixing imports.

--- core-base/core-base/src/main/java/org/nanotek/entities/MutableReleaseGroupPrimaryTypeEntity.java
+++ core-base/core-base/src/main/java/org/nanotek/entities/MutableReleaseGroupPrimaryTypeEntity.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.nanotek.entities.immutables.ReleaseGroupPrimaryTypeEntity;
 
-public interface MutableReleaseGroupPrimaryTypeEntity<K extends Serializable> extends ReleaseGroupPrimaryTypeEntity<K>{
+public interface MutableReleaseGroupPrimaryTypeEntity
+<K extends Serializable> extends ReleaseGroupPrimaryTypeEntity<K>{
 	void setReleaseGroupPrimaryType(K k);
 }
