8fd51719bb322a9f5d4edcffc855e0dacb440c78
Jose Canova <jose.c.canova@gmail.com>
2020-04-01 12:39:40 -0300

    updating model

--- core-base/core-base/src/main/java/org/nanotek/beans/csv/ArtistAliasBean.java
+++ core-base/core-base/src/main/java/org/nanotek/beans/csv/ArtistAliasBean.java
@@ -1,30 +1,15 @@
 package org.nanotek.beans.csv;
 
-import java.util.Optional;
-
-import org.nanotek.Base;
 import org.nanotek.BaseBean;
-import org.nanotek.ImmutableBase;
 import org.nanotek.beans.entity.Artist;
 import org.nanotek.beans.entity.ArtistAlias;
-import org.nanotek.beans.entity.ArtistAliasBeginDate;
-import org.nanotek.beans.entity.ArtistAliasEndDate;
-import org.nanotek.beans.entity.ArtistAliasLocale;
-import org.nanotek.beans.entity.ArtistAliasSortName;
-import org.nanotek.beans.entity.ArtistAliasType;
 import org.nanotek.entities.BaseArtistAliasBean;
-import org.nanotek.entities.MutableAliasIdEntity;
-import org.nanotek.entities.MutableArtistAliasBeginDateEntity;
-import org.nanotek.entities.MutableArtistAliasEndDateEntity;
-import org.nanotek.entities.MutableArtistAliasLocaleEntity;
-import org.nanotek.entities.MutableArtistAliasSortNameEntity;
-import org.nanotek.entities.MutableArtistAliasTypeEntity;
-import org.nanotek.entities.MutableArtistEntity;
-import org.nanotek.entities.MutableArtistIdEntity;
-import org.nanotek.entities.MutableBeginDayEntity;
-import org.nanotek.entities.MutableBeginMonthEntity;
-import org.nanotek.entities.MutableBeginYearEntity;
-import org.nanotek.entities.MutableNameEntity;
+import org.nanotek.entities.BaseArtistAliasBeginDateBean;
+import org.nanotek.entities.BaseArtistAliasEndDateBean;
+import org.nanotek.entities.BaseArtistAliasLocaleBean;
+import org.nanotek.entities.BaseArtistAliasSortNameBean;
+import org.nanotek.entities.BaseArtistAliasTypeBean;
+import org.nanotek.opencsv.ArtistAliasEndDateBean;
 import org.nanotek.proxy.ProxyBase;
 
 //public interface BaseAreaTypeBean<K extends BaseBean<K,AreaType<?>>> 
@@ -34,208 +19,117 @@ import org.nanotek.proxy.ProxyBase;
 //extends CsvBaseBean<ID>
 //implements BaseAreaTypeBean<K>
 public class ArtistAliasBean
-<ID extends ArtistAlias<?>, K extends BaseBean<K,ArtistAlias<?>>> 
-extends ProxyBase<ID>
-implements BaseArtistAliasBean<K>{
+<K extends BaseBean<ArtistAliasBean<K>,ArtistAlias<?>>> 
+extends ProxyBase<ArtistAliasBean<K>,ArtistAlias<?>>
+implements BaseArtistAliasBean<ArtistAliasBean<K>>{
 
 	private static final long serialVersionUID = -2745888243978330408L;
+
+	BaseArtistAliasTypeBean<?> artistAliasType;
 	
-	private ID id; 
+	BaseArtistAliasLocaleBean<?> artistAliasLocale;
 	
-	@Override
-	public ID getId(){ 
-		return id;
-	}
+	BaseArtistAliasBeginDateBean<?> artistAliasBeginDate;
 	
-	public Long aliasId; 
-	public Long artistId; 
-	public String name; 
-	public Long artistAliasLocale; 
-	public Integer editsPending;
-	public String lastUpdated; 
-	public Long artistAliasType; 
-	public String artistAliasSortName;
-	public Integer beginYear; 
-	public Integer beginMonth;
-	public Integer beginDay;
-	public Integer endDateYear; 
-	public Integer endDateMonth;
-	public Integer endDateDay;
-	public String primaryForLocale;
-	public String ended;
+	BaseArtistAliasEndDateBean<?> artistAliasEndDate;
+	
+	BaseArtistAliasSortNameBean<?> artistAliasSortName;
+	
+	Artist<?> artist;
 	
 	
 	public ArtistAliasBean() {
-		super(null);
-	}
-
-	public ArtistAliasBean(Class<ID> id) {
-		super(id);
-	}
-
-	public Long getArtistId() {
-		return artistId;
-	}
-
-
-	public void setArtistId(Long artistId) {
-		this.artistId = artistId;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-	public Integer getEditsPending() {
-		return editsPending;
-	}
-
-
-	public void setEditsPending(Integer editsPending) {
-		this.editsPending = editsPending;
-	}
-
-
-	public String getLastUpdated() {
-		return lastUpdated;
-	}
-
-
-	public void setLastUpdated(String lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
-
-
-	public Integer getEndDateYear() {
-		return endDateYear;
-	}
-
-
-	public void setEndDateYear(Integer endDateYear) {
-		this.endDateYear = endDateYear;
-	}
-
-
-	public Integer getEndDateMonth() {
-		return endDateMonth;
-	}
-
-
-	public void setEndDateMonth(Integer endDateMonth) {
-		this.endDateMonth = endDateMonth;
-	}
-
-
-	public Integer getEndDateDay() {
-		return endDateDay;
-	}
-
-
-	public void setEndDateDay(Integer endDateDay) {
-		this.endDateDay = endDateDay;
-	}
-
-
-	public String getPrimaryForLocale() {
-		return primaryForLocale;
-	}
-
-
-	public void setPrimaryForLocale(String primaryForLocale) {
-		this.primaryForLocale = primaryForLocale;
-	}
-
-
-	public String getEnded() {
-		return ended;
-	}
-
-
-	public void setEnded(String ended) {
-		this.ended = ended;
-	}
-
-
-	@Override
-	public int compareTo(K to) {
-		return withUUID().compareTo(to.withUUID());
+		super((Class<? extends ArtistAlias<?>>) ArtistAlias.class);
+		postConstruct();
 	}
 	
-	@Override
-	public boolean equals(Object obj) {
-			boolean b = Optional.ofNullable(obj).isPresent();
-			if (b) {
-				Base theBase = this.getClass().cast(obj);
-				return this.compareTo(theBase) == 0;}
-			return false;
+	private void postConstruct() {
+		artistAliasType = new ArtistAliasTypeBean<>();
+		
+		 artistAliasLocale = new ArtistAliasLocaleBean<>();
+		
+		artistAliasBeginDate = new ArtistAliasBeginDateBean<> ();
+		
+		 artistAliasEndDate = new ArtistAliasEndDateBean<>();
+		
+		 artistAliasSortName = new ArtistAliasSortNameBean<>();		
+	}
+
+	public ArtistAliasBean(Class<? extends ArtistAlias<?>> class1) {
+		super(class1);
+		postConstruct();
 	}
 	
-	@Override
-	public int hashCode() {
-		return md5Digest().hashCode();
-	}
+	
 
-	public Long getAliasId() {
-		return aliasId;
-	}
-
-	public void setAliasId(Long aliasId) {
-		this.aliasId = aliasId;
-	}
-
-	public String getArtistAliasSortName() {
-		return artistAliasSortName;
-	}
-
-	public void setArtistAliasSortName(String artistAliasSortName) {
-		this.artistAliasSortName = artistAliasSortName;
-	}
-
-	public Long getArtistAliasLocale() {
-		return artistAliasLocale;
-	}
-
-	public void setArtistAliasLocale(Long artistAliasLocale) {
-		this.artistAliasLocale = artistAliasLocale;
-	}
-
-	public Long getArtistAliasType() {
+	public BaseArtistAliasTypeBean<?> getArtistAliasType() {
 		return artistAliasType;
 	}
 
-	public void setArtistAliasType(Long artistAliasType) {
+	public void setArtistAliasType(BaseArtistAliasTypeBean<?> artistAliasType) {
 		this.artistAliasType = artistAliasType;
 	}
 
-	public Integer getBeginYear() {
-		return beginYear;
+	public BaseArtistAliasLocaleBean<?> getArtistAliasLocale() {
+		return artistAliasLocale;
 	}
 
-	public void setBeginYear(Integer beginYear) {
-		this.beginYear = beginYear;
+	public void setArtistAliasLocale(BaseArtistAliasLocaleBean<?> artistAliasLocale) {
+		this.artistAliasLocale = artistAliasLocale;
 	}
 
-	public Integer getBeginMonth() {
-		return beginMonth;
+	public BaseArtistAliasBeginDateBean<?> getArtistAliasBeginDate() {
+		return artistAliasBeginDate;
 	}
 
-	public void setBeginMonth(Integer beginMonth) {
-		this.beginMonth = beginMonth;
+	public void setArtistAliasBeginDate(BaseArtistAliasBeginDateBean<?> artistAliasBeginDate) {
+		this.artistAliasBeginDate = artistAliasBeginDate;
 	}
 
-	public Integer getBeginDay() {
-		return beginDay;
+	public BaseArtistAliasEndDateBean<?> getArtistAliasEndDate() {
+		return artistAliasEndDate;
 	}
 
-	public void setBeginDay(Integer beginDay) {
-		this.beginDay = beginDay;
+	public void setArtistAliasEndDate(BaseArtistAliasEndDateBean<?> artistAliasEndDate) {
+		this.artistAliasEndDate = artistAliasEndDate;
 	}
+
+	public BaseArtistAliasSortNameBean<?> getArtistAliasSortName() {
+		return artistAliasSortName;
+	}
+
+	public void setArtistAliasSortName(BaseArtistAliasSortNameBean<?> artistAliasSortName) {
+		this.artistAliasSortName = artistAliasSortName;
+	}
+
+	public Artist<?> getArtist() {
+		return artist;
+	}
+
+	public void setArtist(Artist<?> artist) {
+		this.artist = artist;
+	}
+
+	
+
+//	
+//	public Long aliasId; 
+//	public Long artistId; 
+//	public String name; 
+//	public Long artistAliasLocale; 
+//	public Integer editsPending;
+//	public String lastUpdated; 
+//	public Long artistAliasType; 
+//	public String artistAliasSortName;
+//	public Integer beginYear; 
+//	public Integer beginMonth;
+//	public Integer beginDay;
+//	public Integer endDateYear; 
+//	public Integer endDateMonth;
+//	public Integer endDateDay;
+//	public String primaryForLocale;
+//	public String ended;
+	
+
 	
 }
--- core-base/core-base/src/main/java/org/nanotek/entities/BaseArtistAliasBean.java
+++ core-base/core-base/src/main/java/org/nanotek/entities/BaseArtistAliasBean.java
@@ -2,13 +2,10 @@ package org.nanotek.entities;
 
 import org.nanotek.Base;
 import org.nanotek.BaseBean;
-import org.nanotek.beans.csv.ArtistAliasBeginDateBean;
+import org.nanotek.beans.csv.ArtistAliasBean;
 import org.nanotek.beans.entity.Artist;
 import org.nanotek.beans.entity.ArtistAlias;
-import org.nanotek.beans.entity.ArtistAliasLocale;
-import org.nanotek.beans.entity.ArtistAliasSortName;
-import org.nanotek.beans.entity.ArtistAliasType;
-import org.nanotek.opencsv.ArtistAliasEndDateBean;
+import org.nanotek.entities.immutables.AliasIdEntity;
 
 
 public interface BaseArtistAliasBean
@@ -16,20 +13,155 @@ public interface BaseArtistAliasBean
 extends BaseBean<K,ArtistAlias<?>>,
 Base<K>,
 MutableAliasIdEntity<Long>,
-MutableArtistAliasSortNameEntity<String>,
+MutableSortNameEntity<String>,
 MutableArtistIdEntity<Long> , 
-MutableArtistAliasTypeEntity<Long> , 
-MutableArtistAliasLocaleEntity<Long>,
+MutableTypeIdEntity<Long> , 
+MutableLocaleEntity<String>,
 MutableNameEntity<String>,
 MutableBeginYearEntity<Integer>,
 MutableBeginMonthEntity<Integer>,
 MutableBeginDayEntity<Integer>,
-MutableArtistAliasSortNameEntity<ArtistAliasSortName<?>>,
-MutableArtistEntity<Artist<?>> , 
+MutableEndYearEntity<Integer>,
+MutableEndMonthEntity<Integer>,
+MutableEndDayEntity<Integer>,
+MutableArtistAliasSortNameEntity<BaseArtistAliasSortNameBean<?>>,
+MutableArtistEntity<Artist<?>>, 
 MutableArtistAliasTypeEntity<BaseArtistAliasTypeBean<?>> , 
 MutableArtistAliasLocaleEntity<BaseArtistAliasLocaleBean<?>>,
 MutableArtistAliasBeginDateEntity<BaseArtistAliasBeginDateBean<?>>,
 MutableArtistAliasEndDateEntity<BaseArtistAliasEndDateBean<?>>
 {
+	@Override
+	default Long getAliasId() {
+		return read(AliasIdEntity.class).map(v->Long.class.cast(v)).orElse(Long.MIN_VALUE);
+	}
+	
+	@Override
+	default void setAliasId(Long k) {
+		write(MutableAliasIdEntity.class,k);
+	}
+	
+	@Override
+	default Long getArtistId() {
+		return getArtist().getArtistId();
+	}
+	
+	@Override
+	default void setArtistId(Long k) {
+		 getArtist().setArtistId(k);
+	}
+	
+	@Override
+	default Long getTypeId() {
+		return getArtistAliasType().getTypeId();
+	}
+	
+	@Override
+	default void setTypeId(Long k) {
+		getArtistAliasType().setTypeId(k);
+	}
 
+	@Override
+	default Integer getBeginDay() {
+		return getArtistAliasBeginDate().getBeginDay();
+	}
+	
+	@Override
+	default void setBeginDay(Integer k) {
+		getArtistAliasBeginDate().setBeginDay(k);
+	}
+	
+	@Override
+	default Integer getBeginMonth() {
+		return getArtistAliasBeginDate().getBeginMonth();
+	}
+	
+	@Override
+	default void setBeginMonth(Integer k) {
+		getArtistAliasBeginDate().setBeginMonth(k);
+	}
+	
+	
+	@Override
+	default Integer getBeginYear() {
+		return getArtistAliasBeginDate().getBeginYear();
+	}
+	
+	@Override
+	default void setBeginYear(Integer k) {
+		getArtistAliasBeginDate().setBeginYear(k);
+	}
+	
+	@Override
+	default Integer getEndDay() {
+		return getArtistAliasEndDate().getEndDay();
+	}
+	
+	@Override
+	default void setEndDay(Integer t) {
+		getArtistAliasEndDate().setEndDay(t);
+	}
+	
+	@Override
+	default Integer getEndMonth() {
+		return getArtistAliasEndDate().getEndMonth();
+	}
+	
+	@Override
+	default Integer getEndYear() {
+		return getArtistAliasEndDate().getEndYear();
+	}
+	
+	@Override
+	default void setEndYear(Integer t) {
+		getArtistAliasEndDate().setEndYear(t);
+	}
+	
+	@Override
+	default void setEndMonth(Integer t) {
+		getArtistAliasEndDate().setEndMonth(t);
+	}
+	
+	@Override
+	default String getName() {
+		return read(NameEntity.class).map(n->String.class.cast(n)).orElse("");
+	}
+	
+	@Override
+	default void setName(String k) {
+		write(MutableNameEntity.class,k);
+	}
+	
+	@Override
+	default void setLocale(String k) {
+		getArtistAliasLocale().setLocale(k);
+	}
+	
+	@Override
+	default String getLocale() {
+		return getArtistAliasLocale().getLocale();
+	}
+	
+	
+	@Override
+	default String getSortName() {
+		return getArtistAliasSortName().getSortName();
+	}
+	
+	@Override
+	default void setSortName(String k) {
+		getArtistAliasSortName().setSortName(k);
+	}
+	
+	
+	public static void main(String[] args) {
+		ArtistAliasBean bean = new ArtistAliasBean();
+		bean.setName("this is a name");
+		System.out.println(bean.getName());
+		bean.setBeginDay(10);
+		System.out.println(bean.getBeginDay());
+	}
+	
+	
+	
 }
