49cc005616e745b0ec0493b1c23d3398e9bc852c
Jose Canova <jose.c.canova@gmail.com>
2020-04-16 09:48:03 -0300

    polishing

--- core-base/core-base/src/main/java/org/nanotek/repository/BaseEntityRepository.java
+++ core-base/core-base/src/main/java/org/nanotek/repository/BaseEntityRepository.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import org.nanotek.BaseEntity;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
