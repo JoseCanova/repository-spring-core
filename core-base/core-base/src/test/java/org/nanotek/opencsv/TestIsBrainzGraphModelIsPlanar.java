b08ee8233d7177e920e3fa0d40dde5d2d079b24f
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-07-14 11:30:42 -0300

    testing planarity of "actual not entirely correct"
    planarity of brainz graph model

---
+++ core-base/core-base/src/test/java/org/nanotek/opencsv/TestIsBrainzGraphModelIsPlanar.java
@@ -0,0 +1,35 @@
+package org.nanotek.opencsv;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.jgrapht.Graph;
+import org.jgrapht.alg.planar.BoyerMyrvoldPlanarityInspector;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.nanotek.BaseEntity;
+import org.nanotek.entities.metamodel.BrainzGraphModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestIsBrainzGraphModelIsPlanar {
+
+	@Autowired
+	BrainzGraphModel brainzGraphModel;
+	
+	@Test
+	void testIsBrainzGraphModelIsPlanar() {
+		assertNotNull(brainzGraphModel);
+		Graph<Class<? extends BaseEntity>, ?> theGraph = brainzGraphModel.getEntityDirectedGraph();
+        BoyerMyrvoldPlanarityInspector<Class<? extends BaseEntity>, ?> planarityTest = 
+        					new BoyerMyrvoldPlanarityInspector<>(theGraph);
+
+        assertTrue(planarityTest.isPlanar());
+	
+	
+	}	
+	
+}
