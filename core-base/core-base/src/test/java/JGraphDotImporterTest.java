58f21116e3f580daa605fe69b85d1604bc5b5fb2
Jose Carlos Canova <jose.c.canova@gmail.com>
2025-07-23 19:57:24 -0300

    update dot importer for simple dot files

--- core-base/core-base/src/test/java/JGraphDotImporterTest.java
+++ core-base/core-base/src/test/java/JGraphDotImporterTest.java
@@ -1,32 +1,42 @@
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.InputStream;
-import java.io.Reader;
+import java.util.Set;
 
 import org.jgrapht.Graph;
-import org.jgrapht.graph.builder.GraphTypeBuilder;
+import org.jgrapht.graph.DefaultDirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.nio.dot.DOTImporter;
 import org.junit.jupiter.api.Test;
 
 
 public class JGraphDotImporterTest<V,E> {
 
-	
+
 	@Test
 	void testDotImporter() {
-		
+
 		InputStream dotStream = this.getClass().getResourceAsStream("decomposed_edges.dot");
 		assertNotNull(dotStream);
-		DOTImporter<V,E> dotImporter = new DOTImporter<>();
-		Graph<V,E> graph = buildEmptySimpleGraph();
-		dotImporter.importGraph(graph, dotStream);
+		DOTImporter<String,DefaultEdge> dotImporter = new DOTImporter<>();
+		dotImporter.setVertexFactory((label) -> {
+			// Custom vertex factory logic (e.g., creating a custom vertex object)
+			System.out.println("Creating vertex: " + label);
+			return label; // Returning the label as the vertex ID in this example
+		});
+		Graph<String, DefaultEdge> graph = buildEmptySimpleGraph();
+		try { 
+			dotImporter.importGraph(graph, dotStream);
+			Set<?> theSet = graph. edgeSet();
+			theSet.forEach(e -> System.out.println(e));
+		}	catch(Exception ex) { 
+			ex.printStackTrace();
+		}
 	}
 
-	private  Graph<V,E> buildEmptySimpleGraph()
+	private  Graph<String, DefaultEdge> buildEmptySimpleGraph()
 	{
-		return GraphTypeBuilder
-				.<V,E>undirected().allowingMultipleEdges(true)
-				.allowingSelfLoops(true).weighted(false).buildGraph();
+		return  new DefaultDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
 	}
 	
 }
